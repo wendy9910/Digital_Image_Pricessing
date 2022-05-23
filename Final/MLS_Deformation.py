1af3647d4d2d59bda8054672c4cdf43524f1e0da
wendy9910 <wendy991017@gmail.com>
2022-05-23 13:39:40 +0800

    結合

---
+++ Final/MLS_Deformation.py
@@ -0,0 +1,217 @@
+import numpy as np
+import cv2     
+
+def eye_deformation(landmarks,img,state):
+    
+    global Leyepts2,Reyepts2,eyeDispts2,LReyePos2,LReyePos1
+    
+    LeyePosCenter = np.uint32([(landmarks[36,0]+landmarks[39,0])/2,(landmarks[36,1]+landmarks[39,1])/2])
+    ReyePosCenter = np.uint32([(landmarks[42,0]+landmarks[45,0])/2,(landmarks[42,1]+landmarks[45,1])/2])
+    
+    Leyepts1 = np.uint32([[landmarks[36,0],landmarks[36,1]],[landmarks[37,0],landmarks[37,1]]
+                       ,[landmarks[38,0],landmarks[38,1]],[landmarks[39,0],landmarks[39,1]]
+                       ,[landmarks[40,0],landmarks[40,1]],[landmarks[41,0],landmarks[41,1]]
+                       ,[(landmarks[36,0]+landmarks[39,0])/2,(landmarks[36,1]+landmarks[39,1])/2]])    
+    
+    Reyepts1 = np.uint32([[landmarks[42,0],landmarks[42,1]],[landmarks[43,0],landmarks[43,1]]
+                       ,[landmarks[44,0],landmarks[44,1]],[landmarks[45,0],landmarks[45,1]]
+                       ,[landmarks[46,0],landmarks[46,1]],[landmarks[47,0],landmarks[47,1]]
+                       ,[(landmarks[42,0]+landmarks[45,0])/2,(landmarks[42,1]+landmarks[45,1])/2]])
+    
+    faceDispts1 = np.uint32([[landmarks[0,0],landmarks[0,1]],[landmarks[4,0],landmarks[4,1]]
+                        ,[landmarks[8,0],landmarks[8,1]],[landmarks[12,0],landmarks[12,1]]
+                        ,[landmarks[16,0],landmarks[16,1]]])
+    
+    eyeDispts1 = faceDispts1.copy()
+    eyeDispts1 = np.append(eyeDispts1,Leyepts1,axis=0)
+    eyeDispts1 = np.append(eyeDispts1,Reyepts1,axis=0)
+    
+    
+    if(state==1):
+        Leyepts2 = eye_deformation_enlarge_Pos(Leyepts1,LeyePosCenter)    
+        Reyepts2 = eye_deformation_enlarge_Pos(Reyepts1,ReyePosCenter)
+    elif(state==2):
+        Leyepts2 = eye_deformation_high_Pos(Leyepts1,LeyePosCenter)    
+        Reyepts2 = eye_deformation_high_Pos(Reyepts1,ReyePosCenter)
+    elif(state==3):
+        Leyepts2 = eye_deformation_distance_Pos(Leyepts1,-1)  
+        Reyepts2 = eye_deformation_distance_Pos(Reyepts1,1)
+
+    if(state==3):
+        eyeDispts2 = faceDispts1.copy()
+        eyeDispts2 = np.append(eyeDispts2,Leyepts2,axis=0)
+        eyeDispts2 = np.append(eyeDispts2,Reyepts2,axis=0)
+        initial = trans(img, eyeDispts1)
+        img3 = initial.deformation(img, eyeDispts2)
+    else:
+        LReyePos1 = Leyepts1.copy()
+        LReyePos1 = np.append(LReyePos1,Reyepts1,axis=0)
+        LReyePos2 = Leyepts2.copy()
+        LReyePos2 = np.append(LReyePos2,Reyepts2,axis=0)
+        initial = trans(img, LReyePos1)
+        img3 = initial.deformation(img, LReyePos2)
+
+      
+#     Leyepts = tuple(map(tuple, Leyepts2))
+#     Reyepts = tuple(map(tuple, Reyepts2))
+#     eyeDispts = tuple(map(tuple, eyeDispts2))
+    
+#     for pos in eyeDispts:
+#         cv2.circle(img3, pos, 5, color=(0, 0, 255),thickness = -1)  
+
+    
+
+    return img3
+
+#調整頂點位置放大縮小
+def eye_deformation_enlarge_Pos(pos1,c):
+    print(c)
+    a = np.empty(shape=(0, 2))
+    #位移方向
+    for idx, point in enumerate(pos1):
+        vec1 = np.int32([pos1[idx]-c])   
+        a = np.append(a,vec1,axis=0)
+    dic = normalization(a)
+    #加移動向量
+    p1 = np.empty(shape=(0, 2))
+    for idx, point in enumerate(pos1):
+        pos = np.uint32([pos1[idx]+dic[idx]*enlarge_value])
+        p1 = np.append(p1,pos,axis=0)
+    p1 = np.uint32(p1)
+    return p1
+
+#調整頂點位置 眼高
+def eye_deformation_high_Pos(pos1,c):
+    a = np.empty(shape=(0, 2))
+    for idx, point in enumerate(pos1):
+        vec1 = np.int32([pos1[idx]-c])   
+        a = np.append(a,vec1,axis=0)
+    dic = normalization(a)
+    p1 = np.empty(shape=(0, 2))
+    #加移動向量
+    for idx, point in enumerate(pos1):
+        if(idx>0 and idx!=3 and idx!=6):
+            pos = np.uint32([pos1[idx]+dic[idx]*enlarge_value])
+            p1 = np.append(p1,pos,axis=0)
+        else:
+            pos = np.uint32([pos1[idx]])
+            p1 = np.append(p1,pos,axis=0)
+        p1 = np.uint32(p1)
+    return p1
+
+def eye_deformation_distance_Pos(pos1,d):
+    dic = np.array([1,0])
+    p1 = np.empty(shape=(0, 2))
+    #加移動向量
+    for idx, point in enumerate(pos1):
+        pos = np.uint32([pos1[idx]+ dic * enlarge_value * d])
+        p1 = np.append(p1,pos,axis=0)
+        p1 = np.uint32(p1)
+    return p1
+
+#規一化後的範圍是[-1, 1]
+def normalization(data):
+    _range = np.max(abs(data))
+    return data / _range
+
+
+class trans():
+    def __init__(self, img, pi):#原圖,原座標組
+        width, height = img.shape[:2]
+        pcth = np.repeat(np.arange(height).reshape(height, 1), [width], axis=1) 
+        pctw = np.repeat(np.arange(width).reshape(width, 1), [height], axis=1).T
+
+        self.img_coordinate = np.swapaxes(np.array([pcth, pctw]), 1, 2).T #維度變換
+        self.cita = compute_G(self.img_coordinate, pi, height, width)
+        self.pi = pi
+        self.W, self.A, self.Z = pre_compute_waz(self.pi, height, width, self.img_coordinate)
+        self.height = height
+        self.width = width
+
+    def deformation(self, img, qi):
+
+        qi = self.pi * 2 - qi
+        mapxy = np.swapaxes(np.float32(compute_fv(qi, self.W, self.A, self.Z, self.height, self.width, self.cita, self.img_coordinate)), 0, 1)
+        img = cv2.remap(img, mapxy[:, :, 0], mapxy[:, :, 1], borderMode=cv2.BORDER_WRAP, interpolation=cv2.INTER_LINEAR)
+
+        return img
+    
+def pre_compute_waz(pi, height, width, img_coordinate):
+    # height*width*控制点个数
+    wi = np.reciprocal(np.power(np.linalg.norm(np.subtract(pi, img_coordinate.reshape(height, width, 1, 2)) + 0.000000001, axis=3),2))
+
+    # height*width*2
+    pstar = np.divide(np.matmul(wi,pi), np.sum(wi, axis=2).reshape(height,width,1))
+
+    # height*width*控制点个数*2
+    phat = np.subtract(pi, pstar.reshape(height, width, 1, 2))
+
+    z1 = np.subtract(img_coordinate, pstar)
+    z2 = np.repeat(np.swapaxes(np.array([z1[:,:,1], -z1[:,:,0]]), 1, 2).T.reshape(height,width,1,2,1), [pi.shape[0]], axis=2)
+
+    # height*width*控制点个数*2*1
+    z1 = np.repeat(z1.reshape(height,width,1,2,1), [pi.shape[0]], axis=2)
+
+    # height*width*控制点个数*1*2
+    s1 = phat.reshape(height,width,pi.shape[0],1,2)
+    s2 = np.concatenate((s1[:,:,:,:,1], -s1[:,:,:,:,0]), axis=3).reshape(height,width,pi.shape[0],1,2)
+
+    a = np.matmul(s1, z1)
+    b = np.matmul(s1, z2)
+    c = np.matmul(s2, z1)
+    d = np.matmul(s2, z2)
+
+    # 重构wi形状
+    ws = np.repeat(wi.reshape(height,width,pi.shape[0],1),[4],axis=3)
+
+    # height*width*控制点个数*2*2
+    A = (ws * np.concatenate((a,b,c,d), axis=3).reshape(height,width,pi.shape[0],4)).reshape(height,width,pi.shape[0],2,2)
+
+    return wi, A, z1
+
+def compute_fv(qi, W, A, Z, height, width, cita, img_coordinate):   
+    qstar = np.divide(np.matmul(W,qi), np.sum(W, axis=2).reshape(height,width,1))
+    qhat = np.subtract(qi, qstar.reshape(height, width, 1, 2)).reshape(height, width, qi.shape[0], 1, 2)
+    fv_ = np.sum(np.matmul(qhat, A),axis=2)
+    fv = np.linalg.norm(Z[:,:,0,:,:],axis=2) / (np.linalg.norm(fv_,axis=3)+0.0000000001) * fv_[:,:,0,:] + qstar
+    fv = (fv - img_coordinate) * cita.reshape(height, width, 1) + img_coordinate
+    return fv
+
+#衰减系数计算
+def compute_G(img_coordinate, pi, height, width, thre = 0.9):
+    # thre: 影响系数，数值越大对控制区域外影响越大，反之亦然，取值范围0到无穷大
+    max = np.max(pi, 0) #返回每列最大元素
+    min = np.min(pi, 0) #返回每列最小元素
+
+    length = np.max(max - min)
+
+    # 计算控制区域中心
+    # p_ = (max + min) // 2
+    p_ = np.sum(pi,axis=0) // pi.shape[0]
+
+    # 计算控制区域
+    minx, miny = min - length
+    maxx, maxy = max + length
+    minx = minx if minx > 0 else 0
+    miny = miny if miny > 0 else 0
+    maxx = maxx if maxx < height else height
+    maxy = maxy if maxy < width else width
+
+    k1 =(p_ - [0,0])[1] / (p_ - [0,0])[0]
+    k2 =(p_ - [height,0])[1] / (p_ - [height,0])[0]
+    k4 =(p_ - [0,width])[1] / (p_ - [0,width])[0]
+    k3 =(p_ - [height, width])[1] / (p_ - [height, width])[0]
+    k = (np.subtract(p_, img_coordinate)[:, :, 1] / (np.subtract(p_, img_coordinate)[:, :, 0] + 0.000000000001)).reshape(height, width, 1)
+    k = np.concatenate((img_coordinate, k), axis=2)
+
+    k[:,:p_[1],0][(k[:,:p_[1],2] > k1) | (k[:,:p_[1],2] < k2)] = (np.subtract(p_[1], k[:,:,1]) / p_[1]).reshape(height, width, 1)[:,:p_[1],0][(k[:,:p_[1],2] > k1) | (k[:,:p_[1],2] < k2)]
+    k[:,p_[1]:,0][(k[:,p_[1]:,2] > k3) | (k[:,p_[1]:,2] < k4)] = (np.subtract(k[:,:,1], p_[1]) / (width - p_[1])).reshape(height, width, 1)[:,p_[1]:,0][(k[:,p_[1]:,2] > k3) | (k[:,p_[1]:,2] < k4)]
+    k[:p_[0],:,0][(k1 >= k[:p_[0],:,2]) & (k[:p_[0],:,2] >= k4)] = (np.subtract(p_[0], k[:,:,0]) / p_[0]).reshape(height, width, 1)[:p_[0],:,0][(k1 >= k[:p_[0],:,2]) & (k[:p_[0],:,2] >= k4)]
+    k[p_[0]:,:,0][(k3 >= k[p_[0]:,:,2]) & (k[p_[0]:,:,2] >= k2)] = (np.subtract(k[:,:,0], p_[0]) / (height - p_[0])).reshape(height, width, 1)[p_[0]:,:,0][(k3 >= k[p_[0]:,:,2]) & (k[p_[0]:,:,2] >= k2)]
+
+    cita = np.exp(-np.power(k[:,:,0] / thre,2))
+#     cita[minx:maxx,miny:maxy] = 1
+    # 如果不需要局部变形，可以把cita的值全置为1
+    # cita = 1
+
+    return cita
---
+++ Final/Main.py
@@ -0,0 +1,161 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+import numpy as np
+import cv2
+import dlib
+from MLS_Deformation import * 
+
+# (對68個點要變動的程式碼，每對imgS做變動，都要呼叫Renew()函式更新)
+
+def OpenCV_small():  # opencv 等比縮小圖片
+    global imgS    
+    nr,nc = imgS.shape[:2]
+    if nr >= 670 or nc >= 670:
+        if nr>= nc:
+            scale = 670/nr 
+        else:
+            scale = 670/nc
+        nr2 = int(nr * scale)
+        nc2 = int(nc * scale)
+        imgS = cv2.resize(imgS,(nc2,nr2),interpolation = cv2.INTER_LINEAR)
+
+def open_file():
+    global imgS,img_show,panel
+    filename=filedialog.askopenfilename()  #獲取文件全路徑
+    imgS = cv2.imread(filename) # 用opencv的方法    
+    OpenCV_small() # opencv 等比縮小圖片
+    
+    # Label改成Canvas，不要用Label因為會有覆寫問題
+    panel = tk.Canvas(block1,width=650,height=600,bg='#FFFFFF') # 設定長寬   
+    imgS = rection(imgS)  # imgS是rection後的opencv圖    
+    Renew() # 更新
+
+def Renew():
+    global imgS,img_show,panel
+    # 把img_show變成PIL格式圖
+    img_show = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    img_show = Image.fromarray(img_show)
+    img_show = ImageTk.PhotoImage(image = img_show)
+    # 要更新
+    panel.delete("all") 
+    panel.create_image(0,0,image=img_show,anchor=NW)
+    panel.place(x=0,y=0)
+
+def rection(img): # OpenCV 測68個點
+    #dlib預測器
+    detector = dlib.get_frontal_face_detector()    #使用dlib庫提供的人臉提取器
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')   #構建特徵提取器
+    # 取灰度
+    img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # 人臉數rects
+    rects = detector(img_gray, 0)
+    for i in range(len(rects)):
+        landmarks = np.matrix([[p.x, p.y] for p in predictor(img,rects[i]).parts()])  #人臉關鍵點識別
+        for idx, point in enumerate(landmarks):        #enumerate函式遍歷序列中的元素及它們的下標
+            # 利用cv2.circle給每個特徵點畫一個圈，共68個
+            cv2.circle(img, pos, 4, color=(0, 255, 0),thickness = -1)
+        
+    return img
+
+
+def get_value(value):
+    print(value)
+    print('eyescale1 set to',eyescale1.get())
+
+"""
+class Scale(tk.Scale):
+    def __init__(self, master=None, **kwargs):
+        tk.Scale.__init__(self, master, **kwargs)
+        self.bind('<Button-1>', self.set_value)
+
+    def set_value(self, event):
+        self.event_generate('<Button-3>', x=event.x, y=event.y)
+"""     
+
+global imgS,img_show,panel
+
+
+   
+window = tk.Tk()
+window.title('人臉五官微調系統')
+window.geometry('960x700')
+
+div_size = 300
+align_mode = 'nswe'
+pad0 = 10
+pad = 5
+
+#GUI整體布局
+blocktop = tk.Frame(window, width=650, height=30)
+blocktop2 = tk.Frame(window, width=div_size, height=30)
+block1 = tk.Frame(window, width=650, height=600,bg='#FFFFFF')
+block2 = tk.Frame(window, width=div_size, height=250)
+block3 = tk.Frame(window, width=div_size, height=250)
+block4 = tk.Frame(window, width=div_size, height=250)
+
+blocktop.grid(column=0, row=0, sticky=align_mode)
+blocktop2.grid(column=1, row=0, sticky=align_mode)
+block1.grid(column=0, row=1, rowspan=4, sticky=align_mode) 
+block2.grid(column=1, row=2, sticky=align_mode)
+block3.grid(column=1, row=3, sticky=align_mode)
+block4.grid(column=1, row=4, sticky=align_mode)
+
+
+#拉霸區塊布局
+eyelabel = tk.Label(block2,text="眼睛",font=('新細明體', 12),padx=pad, pady=pad,fg='#007799')
+font = ('Courier New', 20, 'bold')
+eyescale1 = tk.Scale(
+    block2, label='大小', from_=0, to=100, orient="horizontal"
+    ,tickinterval=10,length=280)
+eyescale1.bind("<ButtonRelease-1>", get_value)
+
+eyescale2 = tk.Scale(
+    block2, label='旋轉', from_=0, to=100, orient="horizontal"
+    ,tickinterval=10,length=280,command=get_value)
+eyescale3 = tk.Scale(
+    block2, label='眼距', from_=0, to=100, orient="horizontal"
+    ,tickinterval=10,length=280,command=get_value)
+
+eyelabel.grid(row=0, column=0)
+eyescale1.grid(row=1, column=0)
+eyescale2.grid(row=2, column=0)
+eyescale3.grid(row=3, column=0)
+
+noselabel = tk.Label(block3,text="鼻子",font=('新細明體', 12),padx=pad, pady=pad,fg='#007799')
+nosescale1 = tk.Scale(
+    block3, label='大小', from_=0, to=100, orient="horizontal",tickinterval=10,length=280)
+nosescale2 = tk.Scale(
+    block3, label='鼻翼', from_=0, to=100, orient="horizontal",tickinterval=10,length=280)
+
+noselabel.grid(row=0, column=0)
+nosescale1.grid(row=1, column=0)
+nosescale2.grid(row=2, column=0)
+
+mouselabel = tk.Label(block4,text="嘴巴",font=('新細明體', 12),padx=pad, pady=pad,fg='#007799')
+mousescale1 = tk.Scale(
+    block4, label='大小', from_=0, to=100, orient="horizontal",tickinterval=10,length=280)
+mousescale2 = tk.Scale(
+    block4, label='薄厚', from_=0, to=100, orient="horizontal",tickinterval=10,length=280)
+
+mouselabel.grid(row=0, column=0)
+mousescale1.grid(row=1, column=0)
+mousescale2.grid(row=2, column=0)
+
+
+#開檔&存檔
+import_btn = tk.Button(blocktop, text='開啟檔案', bg='#BBFFEE', fg='black',height = 1, 
+          width = 20, command=open_file)
+save_btn = tk.Button(blocktop, text="儲存檔案", bg='#BBFFEE', fg='black',height = 1, 
+          width = 20)
+import_btn.grid(column=0, row=0, padx=pad0, pady=pad0, sticky=align_mode)
+save_btn.grid(column=1, row=0, padx=pad0, pady=pad0, sticky=align_mode)
+
+#顯示照片
+mainImage=tk.Label(block1,height = 1, width = 95, image=None,bg='#FFFFFF') 
+mainImage.grid(row=0, column=0, sticky=align_mode)
+
+
+window.mainloop()
